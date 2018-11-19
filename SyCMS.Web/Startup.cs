7497674606bd735722277023e9a0a4a434584c6a
yb123speed <904896209@qq.com>
2018-11-19 16:37:06 +0800

    依赖注入替换被墙的静态资源
    Signed-off-by: yb123speed <904896209@qq.com>

--- SyCMS.Web/Startup.cs
+++ SyCMS.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.ResourceManagement;
 
 namespace SyCMS.Web
 {
@@ -15,6 +16,7 @@ namespace SyCMS.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IResourceManifestProvider, ResourceManifest>();
             services.AddOrchardCms();
         }
 
